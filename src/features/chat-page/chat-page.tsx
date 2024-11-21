bec7ccdca2e64f2286d4b48257e439b47755a723
lucamazzasette <86669886+lucamazzasette@users.noreply.github.com>
2024-11-21 17:01:49 +0100

    Update chat-page.tsx

--- src/features/chat-page/chat-page.tsx
+++ src/features/chat-page/chat-page.tsx
@@ -66,7 +66,7 @@ export const ChatPage: FC<ChatPageProps> = (props) => {
                 }}
                 profilePicture={
                   message.role === "assistant"
-                    ? theme === 'dark' ? "/logo-COMAU-white.png" : "/logo-COMAU.png"
+                    ? theme === 'dark' ? "/logo-COMAU-white.png" : "/ai-icon.png"
                     : session?.user?.image
                 }
                 theme={theme}
